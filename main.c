312548bb549bc55487a397b9962fd986aed78ed4
wagnerbintb <wagner@bintb.com>
2022-05-22 23:35:32 -0300

    app

--- main.c
+++ main.c
@@ -7,8 +7,55 @@ typedef struct{
 	float x, y;
 }Ponto;
 
+void mostra_ponto( void *x ){
+	Ponto *p = x;
+	printf("(%.2f, %.2f)\n", p->x, p->y);
+}
+
+void busca_vizinhos(Ponto p, PilhaGenerica *pilha_vizinhos) {
+  int i ;
+
+  // esquerda
+  if (p.y-1 >= 0) {
+    Ponto p1;
+    p1.x = p.x;
+    p1.y = p.y-1;
+    empilha(pilha_vizinhos, &p1);
+  }
+
+  // cima
+  if (p.x-1 >= 0) {
+    Ponto p2;
+    p2.x = p.x-1;
+    p2.y = p.y;
+    empilha(pilha_vizinhos, &p2);
+  }
+
+  // direita
+  if (p.y+1 <= 5) { // [TODO] Buscar o valor dinamico da matriz
+    Ponto p3;
+    p3.x = p.x;
+    p3.y = p.y+1;
+    empilha(pilha_vizinhos, &p3);
+  }
+
+  // baixo
+  if (p.x+1 <= 6) { // [TODO] Buscar o valor dinamico da matriz
+    Ponto p4;
+    p4.x = p.x+1;
+    p4.y = p.y;
+    empilha(pilha_vizinhos, &p4);
+  }
+
+}
+
+void cria_novo_arquivo_matriz(Matriz m) {
+  
+}
+
 int main(int argc, char *argv[]) {
   Matriz m;
+  Matriz m_modificada;
 
   if( !carrega_arquivo( "entrada.txt", &m ) ){
 		printf("ERRO NO ARQUIVO DE ENTRADA!\n");
@@ -23,23 +70,44 @@ int main(int argc, char *argv[]) {
 	inicializa_pilha( &p1, 20, sizeof( Ponto ) );
 
 
-  int valor;
+  int nova_cor = 5;
   Ponto coord;
 
-  printf("Digite o valor: ");
-  scanf("%d", &valor);
+  coord.x = 3;
+  coord.y = 1;
 
-  printf("Digite as coordenadas (x, y): ");
-  scanf("%d%d", &coord.x, &coord.y);
+  int cor_orig = m.dados[(int)coord.x][(int)coord.y];
   
   empilha(&p1, &coord);
 
   while (pilha_vazia(p1)==0)
   {
-    desempilha();
-  }
-    
+    desempilha(&p1, &coord);
+    if (m.dados[(int)coord.x][(int)coord.y] != nova_cor) {
+      set_valor(&m, (int)coord.x, (int)coord.y, nova_cor);
+      
+      PilhaGenerica pilha_vizinhos;
+	    inicializa_pilha( &pilha_vizinhos, 4, sizeof( Ponto ) );
+      busca_vizinhos(coord, &pilha_vizinhos);
 
+      while (pilha_vazia(pilha_vizinhos)==0)
+      {
+        Ponto vizinho;
+        le_topo(pilha_vizinhos, &vizinho);
+
+        if (m.dados[(int) vizinho.x][(int) vizinho.y] == cor_orig) {
+          empilha(&p1, &vizinho);
+        }
+        desempilha(&pilha_vizinhos, &vizinho);
+      }
+      
+
+      desaloca_pilha( &pilha_vizinhos);
+    }
+  }
+  
+  mostra_matriz( m );
+    
   desaloca_matriz( &m );
   desaloca_pilha( &p1 );
 
